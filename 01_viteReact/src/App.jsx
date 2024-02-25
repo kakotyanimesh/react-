c0fa5362551bf946b8c7022ae0aaf0e928cfa4ce
animesh <phb21013@tezu.ac.in>
2024-02-26 01:25:14 +0530

    fundation of react
    learn how to install react

---
+++ 01_viteReact/src/App.jsx
@@ -0,0 +1,20 @@
+
+function App() {
+
+
+  return (
+    <>
+    <h1>chai aur react | animesh </h1>   
+    <h1>first code i have written in react</h1> 
+    </>
+  )
+}
+
+export default App
+
+// notes from the first class
+// react is a library , which is difference from a framework : in framework rule is must , but in library rule doesnot matter that much 
+// we have to use react@vite : less code then regular react, command is : npm create vite@latest
+// npx : node package execute  
+// npm : node package manager 
+// read the package.json  
