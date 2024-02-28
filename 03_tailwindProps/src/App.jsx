7651df3e6ea988d0f55cc321423b536226a202cd
animesh <phb21013@tezu.ac.in>
2024-02-29 02:41:50 +0530

    basic react
    react is cool

--- 03_tailwindProps/src/App.jsx
+++ 03_tailwindProps/src/App.jsx
@@ -6,12 +6,18 @@ import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+  // let myObject = {
+  //   username: 'ai',
+  //   age: 21
+  // }
+  // let newArr = [1, 2, 4]
 
   return (
     <>
       <h1 className='bg-green-400 text-blue-600 p-4 rounded-xl mb-4'>tailwind test</h1>
-      <Card />
-      <Card />
+      <Card username="animesh" btnText="click me"/>
+      <Card username="kakoty" btnText="visit me "/>
+      <Card username="kakoty" />
      
 
 
