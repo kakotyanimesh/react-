3724bdcb76521b88006631a1e2c7b160c93af65a
animesh <phb21013@tezu.ac.in>
2024-03-04 23:09:51 +0530

    mutw

--- 07_Project/src/componets/Dashboard.jsx
+++ 07_Project/src/componets/Dashboard.jsx
@@ -3,5 +3,6 @@
 export function Dashboard() {
     return <div>
         Dashboard PAGE 
+        <img src="https://images.unsplash.com/photo-1622630732303-8e94514a1746?q=80&w=2053&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
     </div>
 }
